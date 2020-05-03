eb3fe5c29fc2ea0fb49af15ac4a1cb86c199185e
Alexander Khmelov <akhmelev@gmail.com>
2020-05-03 14:01:38 +0300

    comment If not work, set yours app_key openweathermap

--- src/main/java/by/it/example/App.java
+++ src/main/java/by/it/example/App.java
@@ -8,8 +8,14 @@ public class App {
     private App() {
     }
 
+    /**
+     * If not work, set yours app_key openweathermap
+     */
     public static final String APP_KEY = "f9f00838c82ff602fb4841bd9f98e167";
+
     public static final String BASE_URL = "http://api.openweathermap.org/";
+
     public static final WeatherClient client = new WeatherClient();
+
     public static final WeatherService weatherService = new WeatherService(client);
 }
