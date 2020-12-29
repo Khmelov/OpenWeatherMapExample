9239c25c4ba961fc89da589380049338b663210b
Alexander Khmelov <akhmelev@gmail.com>
2020-12-29 10:59:06 +0300

    fix formatter

--- src/main/java/by/it/example/helpers/TemperatureFormatter.java
+++ src/main/java/by/it/example/helpers/TemperatureFormatter.java
@@ -7,13 +7,13 @@ public class TemperatureFormatter {
 
     public static String humanize(double value, String measure) {
         char sign = value < 0 ? '-' : '+';
-        long showValue = Math.round(value);
+        long showValue = Math.abs(Math.round(value));
         return String.format("%s%d%s", sign, showValue, measure);
     }
 
     public static String humanize(double value, char measure) {
         char sign = value < 0 ? '-' : '+';
-        long showValue = Math.round(value);
+        long showValue = Math.abs(Math.round(value));
         return String.format("%s%d°%s", sign, showValue, measure);
     }
 }
